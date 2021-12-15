31a9aff9d2d9c6ab6779406ee53b530ce00b1c3c
chenkeke16190205chen <1042586641@qq.com>
2021-12-15 20:15:38 +0800

    Java基础---面向对象

--- src/main/java/Mail/Mail.java
+++ src/main/java/Mail/Mail.java
@@ -10,18 +10,18 @@ public class Mail {
 
 
     //邮件发送者
-    String sender = "XXXXX@qq.com";
-
-    //装配消息
-    MimeMessage mimeMessage = sender;
-    MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage, true, "UTF-8");
-    messageHelper.setFrom(sender.getUsername(), sender.getNickname());
-    messageHelper.setTo(email);
-    messageHelper.setSubject(template.get("subject"));
-    messageHelper.setText(template.get("content"), true);
-
-    //发送
-    sender.send(mimeMessage);
+//    String sender = "XXXXX@qq.com";
+//
+//    //装配消息
+//    MimeMessage mimeMessage = sender;
+//    MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage, true, "UTF-8");
+//    messageHelper.setFrom(sender.getUsername(), sender.getNickname());
+//    messageHelper.setTo(email);
+//    messageHelper.setSubject(template.get("subject"));
+//    messageHelper.setText(template.get("content"), true);
+//
+//    //发送
+//    sender.send(mimeMessage);
     //添加图片可以成功，但是图片会裂开
     //MimeBodyPart image = new MimeBodyPart();
     //ClassPathResource cpr = new ClassPathResource("src/main/webapp/static/img/mailPic.png");
